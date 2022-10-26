8a3f9758666bd393085c4864daa2379045596798
rafael_nunes <rafa.nuns17@gmail.com>
2022-10-26 14:52:38 -0300

    Estrutura de repeticao WHILE

--- src/java_algaworks/main.java
+++ src/java_algaworks/main.java
@@ -2,28 +2,25 @@ package java_algaworks;
 
 import java.util.Scanner;
 
+
 public class main {
 
     public static void main(String[] args) {
-        
         Scanner entrada = new Scanner(System.in);
         
-        System.out.print("Digite o final da plca: ");
-        int finalPlaca = entrada.nextInt();
+        System.out.print("Digite o numero inicial: ");
+        int numeroInicial = entrada.nextInt();
         
-        switch (finalPlaca) {
-            case 1:
-                System.out.println("Vencimento dia 11 de Janeiro.");
-                break;
-                
-            case 2:
-                System.out.println("Vencimento dia 12 de Janeiro.");
-                break;   
-                default:
-                    System.out.println("Desculpe! Sem registro de tada para o numero informado.");
-            
+        System.out.print("Digite o numero final: ");
+        int numeroFinal = entrada.nextInt();
+        
+        int numeroAtual = numeroInicial;
+        
+        while(numeroAtual <= numeroFinal) { /* WHILE retorna BOOLEAN */
+            System.out.println(numeroAtual);
+            numeroAtual ++;
         }
-        
+            
     }
         
 }
