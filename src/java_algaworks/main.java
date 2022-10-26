03db3a530cfd37e819a084f339deb74623192ed9
rafael_nunes <rafa.nuns17@gmail.com>
2022-10-26 16:06:22 -0300

    Usando for() com divisao de numeros

--- src/java_algaworks/main.java
+++ src/java_algaworks/main.java
@@ -7,22 +7,18 @@ public class main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        int valor = 0;
-        int soma = 0;
+        System.out.print("Digite um numero: ");
+        int divisor = entrada.nextInt();
         
-        do {
-            /*
-            *@ do SEMPRE ESSE CODIGO SERÁ EXECUTADO ANTES DO WHILE
-            * ex: ENQUANTO ELE NAO DIGITAR 0(ZERO) SERA PERGUNTADO UM NUMERO AO USUARIO
-            */
+        for(int i=100; i<=120; i++) {
+            if(i % divisor == 0){ /* 0(zero) significa numero PAR */
+               continue;
+            }
+            System.out.println(i);
             
-            System.out.println("Digite 0 (zero) para sair ou qualquer numero para somar: ");
-            valor = entrada.nextInt();
+        }
+        System.out.println("Fim do programa.");
                 
-            soma += valor;
-            
-        } while (valor != 0);
-            System.out.println("Soma: " + soma);
     }
         
 }
